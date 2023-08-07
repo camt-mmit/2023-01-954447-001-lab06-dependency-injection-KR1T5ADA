a97043a45399e0699f4fcd6776beb2171361bd94
Warodom Techafong <andrewafnz@gmail.com>
2023-08-07 17:51:35 +0700

    lab06

---
+++ src/app/example/dynamic-input/dynamic-input.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputData } from '../types';
+
+@Component({
+  selector: 'app-dynamic-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dynamic-input.component.html',
+  styleUrls: ['./dynamic-input.component.scss'],
+})
+export class DynamicInputComponent {
+  @Input({ required: true }) items!: InputData;
+
+  @Output() readonly dataChange = new EventEmitter();
+
+  isDisabled = true;
+
+  disabled() {
+    if (this.items.length === 1) {
+      this.isDisabled = true;
+    } else {
+      this.isDisabled = false;
+    }
+  }
+
+  onChange(index: number, value: number): void {
+    this.items[index].value = value;
+    this.dataChange.emit();
+  }
+
+  addInput(): void {
+    this.items.push({ value: 0 });
+    this.disabled();
+    this.dataChange.emit();
+  }
+
+  deleteInput(index: number): void {
+    this.items.splice(index, 1);
+    this.disabled();
+    this.dataChange.emit();
+  }
+
+  getResult(): number {
+    return this.items.reduce((prev, curr) => prev + curr.value, 0);
+  }
+}
---
+++ src/app/example/dynamic-section/dynamic-section.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DynamicInputComponent } from '../dynamic-input/dynamic-input.component';
+import { SectionData } from '../types';
+
+@Component({
+  selector: 'app-dynamic-section',
+  standalone: true,
+  imports: [CommonModule, DynamicInputComponent],
+  templateUrl: './dynamic-section.component.html',
+  styleUrls: ['./dynamic-section.component.scss'],
+})
+export class DynamicSectionComponent {
+  @Input({ required: true }) items!: SectionData;
+
+  @Output() dataChange = new EventEmitter<SectionData>();
+
+  isDisabled = true;
+
+  disabled() {
+    if (this.items.length === 1) {
+      this.isDisabled = true;
+    } else {
+      this.isDisabled = false;
+    }
+  }
+
+  addSection(): void {
+    this.items.push([{ value: 0 }]);
+    this.disabled();
+    this.dataChange.emit(this.items);
+  }
+
+  deleteSection(index: number): void {
+    this.items.splice(index, 1);
+    this.disabled();
+    this.dataChange.emit(this.items);
+  }
+
+  onDataChange(): void {
+    this.dataChange.emit(this.items);
+  }
+}
